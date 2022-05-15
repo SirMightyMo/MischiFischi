383dd041e89dc67c31e361b9feee5d9345de343b
Mo Uhlig <mouhlig@gmail.com>
2022-05-15 15:52:32 +0200

    expo update / color picker not working

---
+++ components/ColorTool.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import { SketchPicker } from 'react-color'
+
+export default ColorTool = (props) => {
+ 
+    return <SketchPicker />
+  
+}
--- screens/ShareScreen.js
+++ screens/ShareScreen.js
@@ -1,12 +1,14 @@
 import React from "react";
 import { View, Text } from "react-native";
 import LayoutStyles from "../constants/LayoutStyles";
+import { SketchPicker } from 'react-color'
+import ColorTool from "../components/ColorTool";
 
 export default ShareScreen = () => {
-
   return (
     <View style={LayoutStyles.shareContainer}>
       <Text>share your stuff</Text>
+      <ColorTool/>
     </View>
   );
 };

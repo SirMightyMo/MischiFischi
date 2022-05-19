25cab8ce0c198abc54c9d345a30871ef19f2db54
Leander Wernst <leanderwernst@msn.com>
2022-05-19 16:57:31 +0200

    Console now returns color-adjusted fish-svg; deleted unused data

--- components/Fish.js
+++ components/Fish.js
@@ -1,27 +1,13 @@
+import React, { useContext } from "react";
 import { Dimensions } from 'react-native';
-import React, { useContext} from "react";
+import Svg, { G } from "react-native-svg";
 import { AppContext } from "../data/AppContext";
-import Warp from 'warpjs';
 
-import Svg, { SvgXml, ForeignObject, G, LinearGradient, Defs, Stop, Pattern, Path } from "react-native-svg";
-import Body01 from '../assets/body01'
-import Fin01 from '../assets/fin01'
-import BackFin01 from '../assets/backFin01'
+import Body1 from './fishParts/Body1'; // TESTDATA
+import Fins1 from './fishParts/Fins1'; // TESTDATA
+import Tail1 from './fishParts/Tail1'; // TESTDATA
 
-import Body1 from '../assets/fish/body1';
-import Body2 from '../assets/fish/body2';
-import Body3, {setColorsBody3} from '../components/fishPartsJSX/fishBody1';
-import Backfins1 from '../assets/fish/backfins1';
-import Backfins2 from '../assets/fish/backfins2';
-import Tail1 from '../assets/fish/tail1';
-import Tail2 from '../assets/fish/tail2';
-
-
-export function test() {
-  console.log("test");
-}
-
-export default Fish = ({childToParent}) => {
+export default Fish = () => {
   const [appData, setAppData] = useContext(AppContext);
   const currentFish = appData.fish.find(fish => fish.id === appData.currentId);
 
@@ -29,11 +15,11 @@ export default Fish = ({childToParent}) => {
   const bodyToRender = (currentFish) => {
     switch (currentFish.body) {
       case 0:
-        return <Body1 fillOpacity={1} strokeOpacity={1} fill='#0000FF' strokeWidth={5} stroke={appData.fish[appData.currentId].color} />;
+        return <Body1 gradientCol1={appData.fish[appData.currentId].color1} gradientCol2={appData.fish[appData.currentId].color2} />;
       case 1:
-        return <Body2 fillOpacity={1} strokeOpacity={1} fill='#0000FF' strokeWidth={5} stroke={appData.fish[appData.currentId].color} />;
+        return <Body1 gradientCol1={appData.fish[appData.currentId].color1} gradientCol2={appData.fish[appData.currentId].color2} />;
       case 2:
-        return <Body3 gradientCol1={appData.fish[appData.currentId].color1} gradientCol2={appData.fish[appData.currentId].color2} stroke='#000' />;
+        return <Body1 gradientCol1={appData.fish[appData.currentId].color1} gradientCol2={appData.fish[appData.currentId].color2} />;
       default:
         break;
     }
@@ -42,11 +28,11 @@ export default Fish = ({childToParent}) => {
   const tailToRender = (currentFish) => {
     switch (currentFish.fin) {
       case 0:
-        return <Tail1 fillOpacity={1} strokeOpacity={1} fill='#0000FF' strokeWidth={5} stroke='#FF0000' />;
+        return <Tail1 gradientCol1={appData.fish[appData.currentId].color1} gradientCol2={appData.fish[appData.currentId].color2} />;
       case 1:
-        return <Tail2 fillOpacity={1} strokeOpacity={1} fill='url(#grad)' strokeWidth={5} stroke='#FF0000' />
+        return <Tail1 gradientCol1={appData.fish[appData.currentId].color1} gradientCol2={appData.fish[appData.currentId].color2} />;
       case 2:
-        return <Fin01 fillOpacity={1} strokeOpacity={1} fill='#0000FF' strokeWidth={20} />;
+        return <Tail1 gradientCol1={appData.fish[appData.currentId].color1} gradientCol2={appData.fish[appData.currentId].color2} />;
       default:
         break;
     }
@@ -55,33 +41,39 @@ export default Fish = ({childToParent}) => {
   const backFinToRender = (currentFish) => {
     switch (currentFish.backFin) {
       case 0:
-        return <Backfins1 fillOpacity={1} strokeOpacity={1} fill='#0000FF' strokeWidth={5} stroke='#FF0000' />;
+        return <Fins1 gradientCol1={appData.fish[appData.currentId].color1} gradientCol2={appData.fish[appData.currentId].color2} />;
       case 1:
-        return <Backfins2 fillOpacity={1} strokeOpacity={1} fill='#0000FF' strokeWidth={5} stroke='#FF0000' />;
+        return <Fins1 gradientCol1={appData.fish[appData.currentId].color1} gradientCol2={appData.fish[appData.currentId].color2} />;
       case 2:
-        return <BackFin01 fillOpacity={1} strokeOpacity={1} fill='#0000FF' strokeWidth={20} stroke='#FF0000' />;
+        return <Fins1 gradientCol1={appData.fish[appData.currentId].color1} gradientCol2={appData.fish[appData.currentId].color2} />;
       default:
         break;
     }
   }
 
-   /* Wraps all fish components in one JSX-SVG-Component.
-  TODO: Check scale & positions with final fish-components */
+   /* 
+   Wraps all fish components in one JSX-SVG-Component.
+   TODO: Check scale & positions with final fish-components 
+   TODO: Calculate Scale
+  */
+
+   const windowWidth = Dimensions.get('window').width;
+   const viewBox = "0 0 " + Math.floor(windowWidth) + " " + Math.floor(windowWidth/1.7777777777);
+
   const WrappedFishSvg = () => (
-    <Svg height="100%" width="100%">
-      <G scale="0.5" x="100" y="80">
+    <Svg height="100%" width="100%" viewBox={viewBox}>
+      <G scale="1">
         {bodyToRender(currentFish)}
       </G>
-      <G scale="0.35" x="0" y="140">
+      <G scale="1">
         {tailToRender(currentFish)}
       </G>
-      <G scale="0.8" x="10" y="34">
+      <G scale="1">
         {backFinToRender(currentFish)}
       </G>
     </Svg>
   );
 
-//console.log(finToRender(currentFish))
   return (
       <WrappedFishSvg />
   );

e05368a31428c0c32448bf996aa191fb67282967
Mo Uhlig <mouhlig@gmail.com>
2022-05-30 15:15:02 +0200

    removed unused component

--- screens/BuildScreen.js
+++ screens/BuildScreen.js
@@ -1,17 +1,13 @@
 import React from "react";
-import { View, Text } from "react-native";
+import { View } from "react-native";
 import  { Canvas } from "../components/SvgCanvas";
 import ToolsContainer from "../components/ToolsContainer";
 import LayoutStyles from "../constants/LayoutStyles";
 import { useDimensions } from '@react-native-community/hooks';
-import QrCodeMaker from "../components/QrCodeMaker";
-import QRCode from "react-native-qrcode-svg";
-import Svg from "react-native-svg";
 
 export default BuildScreen = () => {
 
   const { height } = useDimensions().window;
-  //console.log(height)
 
   return (
     <View style={height > 380 ? LayoutStyles.buildContainerVertical : LayoutStyles.buildContainerHorizonatal} >
